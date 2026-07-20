307a328f9618202f8cce3dd00f6d1957c583391b
Pakawat Sangchantra <pakawat_sangchantra@cmu.ac.th>
2026-07-20 07:27:53 +0700

    15.3

---
+++ src/views/EventListView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import EventMeta from '@/components/EventMeta.vue'
+import type { Event } from '@/types'
+import { ref, onMounted } from 'vue'       // added onMounted
+import axios from 'axios'                  // added
+
+const events = ref<Event[] | null>(null)   // changed from array to null
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Tor4447/672115033/events')
+    .then((response) => {
+      console.log(response.data)           // show data in console
+      events.value = response.data         // assign to events
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <div class="home">
+    <h1>Events For Good</h1>
+    <div class="events">
+      <!-- Show loading or empty state if events is null -->
+      <div v-if="!events">Loading events...</div>
+      <template v-else v-for="event in events" :key="event.id">
+        <EventCard :event="event" />
+        <EventMeta :event="event" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
